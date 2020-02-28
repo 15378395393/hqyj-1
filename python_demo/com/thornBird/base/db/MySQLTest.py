89675631da52bdad30179516a0fd315a8cdc5a92
hymanHu <hujiangyx@163.com>
2020-02-28 16:56:32 +0800

    "test"

--- python_demo/com/thornBird/base/db/MySQLTest.py
+++ python_demo/com/thornBird/base/db/MySQLTest.py
@@ -8,7 +8,8 @@ def mySQLTest():
     # 增删改操作
     cursor = conn.cursor();
     cursor.execute('DROP TABLE IF EXISTS `user`;');
-    cursor.execute('create table user (id varchar(20) primary key, name varchar(20))');
+    # cursor.execute('create table user (id varchar(20) primary key, name varchar(20))');
+    cursor.execute('create table user (id INT AUTO_INCREMENT PRIMARY KEY, name varchar(20))');
     # 插入一行记录，注意MySQL的占位符是%s:
     cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'HymanHu']);
     cursor.execute('insert into user (id, name) values (%s, %s)', ['2', 'Jianghu']);
